47c049d170887a8b4966a19c6102badd3a882e7d
Jian Aldrich Soriano <jianaldrichsoriano@gmail.com>
2022-09-05 19:39:01 +0800

    ADD initial commit for React lesson

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! I am now a React Guru (In the making)!
         </p>
         <a
           className="App-link"
